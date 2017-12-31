4ff9af8e13316f237d12e23134f670fa9dc25f92
SMaitland <samsub@comcast.net>
2017-12-31 12:33:16 -0500

    Ready to submit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function makeCacheMatrix creates a list of 4 functions
+## to set and get the inverse of a matrix, x
+## The assignment allows for the assumption that the matrix x 
+## is invertible so no error handling is included
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Reset the matrix inverse
+    xinv <- NULL
+    # Function to set the value of the matrix 
+    # And reset the value of the inverse of the matrix to NULL
+    set <- function(y) {
+        x <<- y
+        xinv <<- NULL
+    }
+    # Function to get the value of the matrix
+    get <- function() x
+    # Function to set the value of the inverse of the matrix
+    setinv <- function(i) xinv <<- i
+    # Function to get the value of the inverse of the matrix
+    getinv <- function() xinv
+    # Create the list
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve
+## Checks for a current value for the inverse of the matrix
+## If xinf has a value, then return it
+## If not, calculate and set itlibrary(swirl)library(swirl)library(swirl)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    xinv <- x$getinv()
+    if(!is.null(xinv)) {
+        message("getting cached data")
+        return(xinv)
+    }
+    # If not, then calculate and set it
+    data <- x$get()
+    xinv <- solve(data, ...)
+    x$setinv(xinv)
+    xinv
+} 
